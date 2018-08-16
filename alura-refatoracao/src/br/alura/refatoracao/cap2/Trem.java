970afae83d6bdadbd3bb832108a5f0185ad04e57
AndreLucasrs <andrelrs.v@outlook.com>
2018-08-16 17:47:17 -0300

    Aula 2

--- alura-refatoracao/src/br/alura/refatoracao/cap2/Trem.java
+++ alura-refatoracao/src/br/alura/refatoracao/cap2/Trem.java
@@ -2,20 +2,29 @@ package br.alura.refatoracao.cap2;
 
 import java.util.List;
 
+//Nomes adequados facilitam a leitura e manuntenção dos códigos
 public class Trem {
 
 	private List<Vagao> vagoes;
 	private int capacidade;
 	
-	public boolean disp(int reservas) {
+	//variavel so pode ter um conceito do ponto de vista semantico
+	//exemplo, se ela é feito pra somar ela so pode somar
+	//ela so vai representar uma unica coisa no codigo
+	//ela não pode ficar mudando ao longo do tempo
+	public boolean podeReservar(int lugaresAReservar) {
 		
-		int r = 0;
-		for(Vagao v : vagoes) {
-			r += v.reservados();
+		int lugaresDisponiveis = capacidade - lugaresJaReservados();
+		return lugaresDisponiveis > lugaresAReservar; 
+	}
+
+	private int lugaresJaReservados() {
+		
+		int lugaresJaReservados = 0;
+		for(Vagao vagao : vagoes) {
+			lugaresJaReservados += vagao.reservados();
 		}
-		
-		r = capacidade - r;
-		return r > reservas; 
+		return lugaresJaReservados;
 	}
 	
 }
